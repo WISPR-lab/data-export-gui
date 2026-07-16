c3e7cd0f5b81e77306e5184045e776fbf3534aac
julia <95244239+julianonn@users.noreply.github.com>
2026-07-16 16:23:04 -0500

    no text hover on filter chip

--- webapp/src/components/Events/FilterChip.vue
+++ webapp/src/components/Events/FilterChip.vue
@@ -1,40 +1,34 @@
 <template>
   <v-menu offset-y content-class="menu-with-gap">
     <template v-slot:activator="{ on: onMenu }">
-      <v-tooltip top :disabled="formatChipDisplay(chip).length < 33" open-delay="300">
-        <template v-slot:activator="{ on: onTooltip, attrs }">
-          <v-chip
-            outlined
-            close
-            close-icon="mdi-close"
-            @click:close="$emit('remove', chip)"
-            v-on="{ ...onMenu, ...onTooltip }"
-            v-bind="attrs"
-          >
-            <!-- Icon for time chips -->
-            <v-icon v-if="isTimeChip" left small>mdi-clock-outline</v-icon>
+      <v-chip
+        outlined
+        close
+        close-icon="mdi-close"
+        @click:close="$emit('remove', chip)"
+        v-on="onMenu"
+      >
+        <!-- Icon for time chips -->
+        <v-icon v-if="isTimeChip" left small>mdi-clock-outline</v-icon>
 
-            <!-- Icons for term/tag/label chips -->
-            <v-icon v-else-if="chip.value === '__ts_star'" left small color="amber">mdi-star</v-icon>
-            <v-icon v-else-if="chip.value === '__ts_comment'" left small>mdi-comment-multiple-outline</v-icon>
-            <v-icon v-else-if="getQuickTag(chip.value)" left small :color="getQuickTag(chip.value).color">{{
-              getQuickTag(chip.value).label
-            }}</v-icon>
+        <!-- Icons for term/tag/label chips -->
+        <v-icon v-else-if="chip.value === '__ts_star'" left small color="amber">mdi-star</v-icon>
+        <v-icon v-else-if="chip.value === '__ts_comment'" left small>mdi-comment-multiple-outline</v-icon>
+        <v-icon v-else-if="getQuickTag(chip.value)" left small :color="getQuickTag(chip.value).color">{{
+          getQuickTag(chip.value).label
+        }}</v-icon>
 
-            <!-- Text styling based on active state -->
-            <span v-bind:style="[chip.active === false ? { 'text-decoration': 'line-through', opacity: '50%' } : '']">
-              <span v-if="chip.operator === 'must_not'" class="filter-chip-truncate">
-                <span style="color: red">NOT </span>
-                {{ formatChipDisplay(chip) }}
-              </span>
-              <span v-else class="filter-chip-truncate">
-                {{ formatChipDisplay(chip) }}
-              </span>
-            </span>
-          </v-chip>
-        </template>
-        <span>{{ chip.value }}</span>
-      </v-tooltip>
+        <!-- Text styling based on active state -->
+        <span v-bind:style="[chip.active === false ? { 'text-decoration': 'line-through', opacity: '50%' } : '']">
+          <span v-if="chip.operator === 'must_not'" class="filter-chip-truncate">
+            <span style="color: red">NOT </span>
+            {{ formatChipDisplay(chip) }}
+          </span>
+          <span v-else class="filter-chip-truncate">
+            {{ formatChipDisplay(chip) }}
+          </span>
+        </span>
+      </v-chip>
     </template>
     <v-card>
       <v-list dense>
@@ -50,39 +44,39 @@
           style="overflow: visible"
         >
           <template v-slot:activator="{ on, attrs }">
-            <v-list-item v-bind="attrs" v-on="on">
+                <v-list-item v-bind="attrs" v-on="on">
+                  <v-list-item-action class="mr-3">
+                    <v-icon>mdi-square-edit-outline</v-icon>
+                  </v-list-item-action>
+                  <v-list-item-subtitle>Edit filter</v-list-item-subtitle>
+                </v-list-item>
+              </template>
+              <filter-menu app :selected-chip="chip" @updateChip="$emit('update', $event, chip)"></filter-menu>
+            </v-menu>
+
+            <v-list-item v-else @click="$emit('toggle-operator', chip)">
               <v-list-item-action class="mr-3">
-                <v-icon>mdi-square-edit-outline</v-icon>
+                <v-icon v-if="chip.operator === 'must_not'">mdi-plus-circle-outline</v-icon>
+                <v-icon v-else>mdi-minus-circle-outline</v-icon>
               </v-list-item-action>
-              <v-list-item-subtitle>Edit filter</v-list-item-subtitle>
+              <v-list-item-subtitle>
+                <span v-if="chip.operator === 'must_not'">Include results</span>
+                <span v-else>Exclude results</span>
+              </v-list-item-subtitle>
             </v-list-item>
-          </template>
-          <filter-menu app :selected-chip="chip" @updateChip="$emit('update', $event, chip)"></filter-menu>
-        </v-menu>
 
-        <v-list-item v-else @click="$emit('toggle-operator', chip)">
-          <v-list-item-action class="mr-3">
-            <v-icon v-if="chip.operator === 'must_not'">mdi-plus-circle-outline</v-icon>
-            <v-icon v-else>mdi-minus-circle-outline</v-icon>
-          </v-list-item-action>
-          <v-list-item-subtitle>
-            <span v-if="chip.operator === 'must_not'">Include results</span>
-            <span v-else>Exclude results</span>
-          </v-list-item-subtitle>
-        </v-list-item>
-
-        <!-- Option 2: Temporarily disable / Re-enable (Common) -->
-        <v-list-item @click="$emit('toggle', chip)">
-          <v-list-item-action class="mr-3">
-            <v-icon v-if="chip.active !== false">mdi-eye-off</v-icon>
-            <v-icon v-else>mdi-eye</v-icon>
-          </v-list-item-action>
-          <v-list-item-subtitle>
-            <span v-if="chip.active !== false">Temporarily disable</span>
-            <span v-else>Re-enable</span>
-          </v-list-item-subtitle>
-        </v-list-item>
-
+          <!-- Option 2: Temporarily disable / Re-enable (Common) -->
+          <v-list-item @click="$emit('toggle', chip)">
+            <v-list-item-action class="mr-3">
+              <v-icon v-if="chip.active !== false">mdi-eye-off</v-icon>
+              <v-icon v-else>mdi-eye</v-icon>
+            </v-list-item-action>
+            <v-list-item-subtitle>
+              <span v-if="chip.active !== false">Temporarily disable</span>
+              <span v-else>Re-enable</span>
+            </v-list-item-subtitle>
+            </v-list-item>
+            
         <!-- Option 3: Remove filter (Common) -->
         <v-list-item @click="$emit('remove', chip)">
           <v-list-item-action class="mr-3">
